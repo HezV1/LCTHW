5b88b9456a5b312cce5f6306002b1223c6ba97c5
Hez <johndeereboy1997@gmail.com>
2014-06-11 20:11:50 -0500

    1-5

---
+++ ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Warning: This program is wrong on purpose. */
+
+int main()
+{
+	int age = 10;
+	int height = 32;
+	
+	printf("I am %d years old. \n", age);
+	printf("I am %d inhchess tall.\n", height);
+	
+	return 0;
+}
